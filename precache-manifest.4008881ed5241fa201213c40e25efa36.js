6b9245250508a97d3b0f3b0a8b431b0f6f284f70
luvisoniasty <luvisonx@gmail.com>
2019-06-25 18:50:48 +0200

    Updates

---
+++ precache-manifest.4008881ed5241fa201213c40e25efa36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "becf73d43dadf525f26ea59412fe15d1",
+    "url": "/neoteric-recruitment-task/index.html"
+  },
+  {
+    "revision": "d69d48a8b706647ad23b",
+    "url": "/neoteric-recruitment-task/static/js/2.c59e0196.chunk.js"
+  },
+  {
+    "revision": "5eb3c350d39b3d8e10c3",
+    "url": "/neoteric-recruitment-task/static/js/main.0e779261.chunk.js"
+  },
+  {
+    "revision": "b09a0178b397d2b4cd30",
+    "url": "/neoteric-recruitment-task/static/js/runtime~main.baac5a4f.js"
+  },
+  {
+    "revision": "2b4798f5c303bb0d9001413af970a22b",
+    "url": "/neoteric-recruitment-task/static/media/pokeball.2b4798f5.svg"
+  }
+]);
